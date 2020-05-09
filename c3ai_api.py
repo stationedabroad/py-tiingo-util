832410ca82048e4fe559e8886803772743ab642f
sulman shaikh <sulmanmh@gmail.com>
2020-05-09 22:26:03 +0100

    c3aiapi example resource call

---
+++ c3ai_api.py
@@ -0,0 +1,24 @@
+import requests
+
+countries = ["Poland"]
+metrics = ["ECDC_ConfirmedCases"]
+
+headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+}
+
+resource = "https://api.c3.ai/covid/api/1/outbreaklocation/evalmetrics"
+
+query = {
+    "spec": {
+        "ids": countries,
+        "expressions": metrics,
+        "interval": "DAY",
+        "start": "2020-01-01",
+        "end": "2020-05-09"
+    }
+}
+
+
+data_response = requests.post(resource, json=query, headers=headers)
