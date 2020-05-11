611baba18a2ca8213e9cfcb3842b2cad5eed4bcb
sulman shaikh <sulmanmh@gmail.com>
2020-05-11 23:22:29 +0100

    adding writing of api calls to json docs, per country

--- c3ai_api.py
+++ c3ai_api.py
@@ -1,10 +1,12 @@
 import requests
+import ujson
 
 # add countries
-countries = ["Poland"]
+countries = [location for location in open("data/locations.csv", "r").readlines()]
+countries = ['Poland']
 
 # add metrics: ECDC_ConfirmedCases, ECDC_ConfirmedDeaths for example from ECDC (amongst others)
-metrics = ["ECDC_ConfirmedCases"]
+metrics = ["ECDC_ConfirmedCases", "ECDC_ConfirmedDeaths"]
 
 headers = {
     "Accept": "application/json",
@@ -19,9 +21,13 @@ query = {
         "expressions": metrics,
         "interval": "DAY",
         "start": "2020-01-01",
-        "end": "2020-05-09"
+        "end": "2020-05-11"
     }
 }
-
-
+query = dict(query)
 data_response = requests.post(resource, json=query, headers=headers)
+
+if data_response.status_code == 200:
+    f = open(f"data/{countries[0]}.json", "w")
+    f.writelines(data_response.json())
+    f.close()
