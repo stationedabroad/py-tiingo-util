f3679289bbf8bf2bfb4bdfde674a442e6e974218
sulman shaikh <sulmanmh@gmail.com>
2020-05-29 18:53:47 +0100

    Amended api read with cli entry, manual entry and default entry

--- c3ai_api.py
+++ c3ai_api.py
@@ -1,10 +1,11 @@
 import requests
+import sys
 import ujson
 
 from covid19 import get_covid_country_data
 
 # add countries
-countries = [location for location in open("data/locations.csv", "r").readlines()]
+# countries = [location for location in open("data/locations.csv", "r").readlines()]
 
 # add metrics: ECDC_ConfirmedCases, ECDC_ConfirmedDeaths for example from ECDC (amongst others)
 metrics = ["ECDC_ConfirmedCases", 
@@ -31,18 +32,33 @@ headers = {
 
 resource = "https://api.c3.ai/covid/api/1/outbreaklocation/evalmetrics"
 
-for country in countries:
-    for metric in metrics:
+def main(countries=None):
+    for country in countries:
+        for metric in metrics:
 
-        query = {
-            "spec": {
-                "ids": [country],
-                "expressions": [metric],
-                "interval": "DAY",
-                "start": "2020-01-01",
-                "end": "2020-05-14"
+            query = {
+                "spec": {
+                    "ids": [country],
+                    "expressions": [metric],
+                    "interval": "DAY",
+                    "start": "2020-01-01",
+                    "end": "2020-05-14"
+                }
             }
-        }
+            res = get_covid_country_data(resource, country, query, metric)
+            print(f"country data: {country} status: {res.status_code}")
 
-    res = get_covid_country_data(resource, country, query, metric)
-    print(f"country data: {country} status: {res.status_code}")
+if __name__ == '__main__':
+    # get country loc list
+    countries = [location.rstrip('\n') for location in open("data/locations.csv", "r").readlines()]
+    # print(countries)
+    if len(sys.argv) > 1:
+        for cli_country in sys.argv[1:]:
+            if cli_country not in countries:
+                raise ValueError(f"{cli_country} not valid location for api")
+        main(sys.argv[1:])
+    else:
+        print("Enter particular country (comma separated)")
+        cli_countries = list(input().split(','))
+        cli_countries = cli_countries or countries
+        main(countries=cli_countries)
