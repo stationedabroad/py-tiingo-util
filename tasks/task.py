3c0a470bd9deb2ce9a46b862046fa5b862db568f
sulman shaikh <sulmanmh@gmail.com>
2020-06-07 23:00:16 +0100

    as dict task test added

--- tasks/task.py
+++ tasks/task.py
@@ -14,3 +14,14 @@ def test_member_access():
     assert t.summary == 'start server'
     assert t.owner == 'mahmoods'
     assert t.done == False and t.id == None
+
+def test_asdict():
+    t_task = Task('reset server', 'mahmoods', True, 212)    
+    d_task = t_task._asdict()
+    expected = {
+        'summary': 'reset server',
+        'owner': 'mahmoods',
+        'done': True,
+        'id': 212
+    }
+    assert d_task == expected
