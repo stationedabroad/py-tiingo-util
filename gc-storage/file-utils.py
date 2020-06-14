7c14e5856ce2852b77875d5e9d18c21538e64eab
sulman shaikh <sulmanmh@gmail.com>
2020-06-14 21:46:45 +0100

    remove gcloud creds - error

--- gc-storage/file-utils.py
+++ gc-storage/file-utils.py
@@ -1,5 +1,8 @@
 from io import BytesIO
 from hashlib import md5
 from gcloud import storage
+from pathlib import Path
 
-print(storage)
+# to use credentials drop gc-cloud service accoutn creds into the filder hich this __file__ resides in
+print(__file__)
+print(Path(__file__).parent.resolve())
