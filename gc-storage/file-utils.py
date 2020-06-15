8e71d9c469bd8b34b8510b07b031f1c233e1c558
sulman shaikh <sulmanmh@gmail.com>
2020-06-15 22:42:34 +0100

    gitignore update with file-utils comments clean up

--- gc-storage/file-utils.py
+++ gc-storage/file-utils.py
@@ -4,14 +4,12 @@ from hashlib import md5
 from gcloud import storage
 from pathlib import Path
 
+# to use credentials drop gc-cloud service accoutn creds into the filder hich this __file__ resides in
 APP_PROJECT = os.getenv("GCLOUD_PROJECT")
 APP_CREDENTIALS_JSON = os.getenv("GCLOUD_STORAGE_CRED")
 
 my_resource_bucket = "covid19-datafiles"
 
-# to use credentials drop gc-cloud service accoutn creds into the filder hich this __file__ resides in
-print(f"Running ... {Path(__file__).parent.resolve() / __file__}")
-print(APP_CREDENTIALS_JSON, APP_PROJECT)
 
 class FileProcessor:
     def __init__(self, resource_path):
