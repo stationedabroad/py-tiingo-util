f50ae0f1daed1b67056c12bc990bee505bd341d8
sulman shaikh <sulmanmh@gmail.com>
2020-06-15 22:38:56 +0100

    skeleton added for FileProcessor - blob

--- gc-storage/file-utils.py
+++ gc-storage/file-utils.py
@@ -7,14 +7,20 @@ from pathlib import Path
 APP_PROJECT = os.getenv("GCLOUD_PROJECT")
 APP_CREDENTIALS_JSON = os.getenv("GCLOUD_STORAGE_CRED")
 
+my_resource_bucket = "covid19-datafiles"
+
 # to use credentials drop gc-cloud service accoutn creds into the filder hich this __file__ resides in
 print(f"Running ... {Path(__file__).parent.resolve() / __file__}")
 print(APP_CREDENTIALS_JSON, APP_PROJECT)
 
-client = storage.Client.from_service_account_json(json_credentials_path=APP_CREDENTIALS_JSON, project=APP_PROJECT) 
-bucket = client.get_bucket("covid19-datafiles")
-for blob in bucket.list_blobs():
-    print(dir(blob))
-    print(blob.md5_hash)
-    print(blob.name)
-    break
+class FileProcessor:
+    def __init__(self, resource_path):
+        self.resource_path = resource_path
+        self.client = storage.Client.from_service_account_json(json_credentials_path=APP_CREDENTIALS_JSON, project=APP_PROJECT) 
+        self.bucket = self.client.get_bukcet(self.resource_path)
+
+    def run(self):
+        pass
+
+    def process_nlob(self):
+        pass
