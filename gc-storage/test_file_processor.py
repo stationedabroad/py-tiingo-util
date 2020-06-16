0b2fed2ec798a02855efe9036509a79f3aa1bf4a
sulman shaikh <sulmanmh@gmail.com>
2020-06-16 23:30:41 +0100

    test file processor stud amnded to include mocks

--- gc-storage/test_file_processor.py
+++ gc-storage/test_file_processor.py
@@ -1,8 +1,6 @@
 from unittest.mock import Mock
-import json
+from unittest import TestCase
+from file_utils import FileProcessor
 
-json = Mock()
-json.dumps({'a': 1})
-json.dumps({'a': 1})
-print(json.dumps.assert_called())
-print(json.dumps.call_count)
+class TestFileProcessor(TestCase):
+    pass
