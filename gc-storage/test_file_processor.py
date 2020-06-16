4d0b41af6f6c875d9225e9666ddb795999c27213
sulman shaikh <sulmanmh@gmail.com>
2020-06-16 23:38:04 +0100

    failing test case for md5 blob test

--- gc-storage/test_file_processor.py
+++ gc-storage/test_file_processor.py
@@ -1,6 +1,17 @@
 from unittest.mock import Mock
+from unittest.mock import patch
 from unittest import TestCase
 from file_utils import FileProcessor
 
 class TestFileProcessor(TestCase):
-    pass
+    def test_run_returns_md5(self):
+        with patch('file_processor.storage'):
+            fp = FileProcessor('some/bucket')
+            with patch.object(fp, 'bucket') as mock_bucket:
+                mock_blob = Mock(name='mock_blob')
+                def mock_blob_download_to_file(bytez):
+                    bytez.write(b'arrangeinserver')
+                mock_blob.download_to_file = mock_blob_download_to_file
+                mock_bucket.list_blobs.return_value = [mock_blob]
+                result = fp.run()
+                self.assertEqual(result, ['111'])
