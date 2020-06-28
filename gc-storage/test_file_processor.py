0e33afe134b4e100534f132b3c8dff45b3ba331c
Sulman <sulmanmh@gmail.com>
2020-06-28 18:31:22 +0100

    test program for new ABC file prcoessor

--- gc-storage/test_file_processor.py
+++ gc-storage/test_file_processor.py
@@ -1,21 +1,20 @@
+from io import BytesIO
 from unittest import main as testmain
 from unittest.mock import Mock
 from unittest.mock import patch
 from unittest import TestCase
-from file_utils import FileProcessor
+
+from file_processor import FileProcessor
+from file_abc import FileRepoABC
 
 class TestFileProcessor(TestCase):
-    def test_run_returns_md5(self):
-        with patch('file_utils.storage'):
-            fp = FileProcessor('some/bucket')
-            with patch.object(fp, 'bucket') as mock_bucket:
-                mock_blob = Mock(name='mock_blob')
-                def mock_blob_download_to_file(bytez):
-                    bytez.write(b'arrangeinserver')
-                mock_blob.download_to_file = mock_blob_download_to_file
-                mock_bucket.list_blobs.return_value = [mock_blob]
-                result = fp.run()
-                self.assertEqual(result, ['53a195336dfdd9034b8371408f2d7729'])
+    def test_run_file_hasher(self):
+        mock_repo = Mock(spec=FileRepoABC)
+        mock_repo.get_files.return_value = [BytesIO(b"foo"), BytesIO(b"bar")]
+        fp = FileProcessor(mock_repo, None)
+        results = fp.run()
+        self.assertEqual(results, ['acbd18db4cc2f85cedef654fccc4a4d8', '37b51d194a7513e45b56f6524f2d51f2'])
+
 
 if __name__ == '__main__':
     testmain()                
---
+++ gc-storage/test_file_utils.py
@@ -0,0 +1,21 @@
+from unittest import main as testmain
+from unittest.mock import Mock
+from unittest.mock import patch
+from unittest import TestCase
+from file_utils import FileProcessor
+
+class TestFileProcessor(TestCase):
+    def test_run_returns_md5(self):
+        with patch('file_utils.storage'):
+            fp = FileProcessor('some/bucket')
+            with patch.object(fp, 'bucket') as mock_bucket:
+                mock_blob = Mock(name='mock_blob')
+                def mock_blob_download_to_file(bytez):
+                    bytez.write(b'arrangeinserver')
+                mock_blob.download_to_file = mock_blob_download_to_file
+                mock_bucket.list_blobs.return_value = [mock_blob]
+                result = fp.run()
+                self.assertEqual(result, ['53a195336dfdd9034b8371408f2d7729'])
+
+if __name__ == '__main__':
+    testmain()                
