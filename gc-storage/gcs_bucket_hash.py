11f88280c9a3c556bc307695cfd4ec48bdf9341f
Sulman <sulmanmh@gmail.com>
2020-06-28 18:16:40 +0100

    GCS file hasher test program, using file assets

---
+++ gc-storage/gcs_bucket_hash.py
@@ -0,0 +1,16 @@
+import os
+from file_processor import FileProcessor
+from file_gcs import GCSFileRepo
+
+APP_PROJECT = os.getenv("GCLOUD_PROJECT")
+APP_CREDENTIALS_JSON = os.getenv("GCLOUD_STORAGE_CRED")
+COVID_BUCKET = "covid19-datafiles"
+
+def main():
+    gcs_repo = GCSFileRepo(APP_CREDENTIALS_JSON, COVID_BUCKET)
+    fp = FileProcessor(gcs_repo, None)
+    gcs_bucket_hashes = fp.run()
+    print(gcs_bucket_hashes)
+
+if __name__ == '__main__':
+    main()
