cd7d5c28906ba52ec625d6d21bce08b2da0d49e0
sulman shaikh <sulmanmh@gmail.com>
2020-07-12 16:58:52 +0100

    some mypy typing tests - headline func@

---
+++ utils/header.py
@@ -0,0 +1,7 @@
+def headline(text: str, centered: bool = False) -> str:
+    if not centered:
+        return f"{text.title()}\n{'-' * len(text)}"
+    return f" {text.title()} ".center(50, " ")
+
+print(headline("some type checking"))
+print(headline("some more type chekcing with mypy", centered=True))
