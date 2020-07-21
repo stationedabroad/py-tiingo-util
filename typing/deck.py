03b77ca7e9ab4bdc4bfda57ad8a2ebcf4b23ba04
sulman shaikh <sulmanmh@gmail.com>
2020-07-21 23:06:18 +0100

    custom type annots for Deck and Card

--- typing/deck.py
+++ typing/deck.py
@@ -9,14 +9,17 @@ from typing import (
 SUITS = "♠ ♡ ♢ ♣".split()
 RANKS = "2 3 4 5 6 7 8 9 10 J Q K A".split()
 
-def create_deck(shuffle: bool = False) -> List[Tuple[str, str]]:
+Card = Tuple[str, str]
+Deck = List[Card]
+
+def create_deck(shuffle: bool = False) -> Deck:
     """Create a new deck of 52 cards"""
     deck = [(s, r) for r in RANKS for s in SUITS]
     if shuffle:
         random.shuffle(deck)
     return deck
 
-def deal_hands(deck):
+def deal_hands(deck) -> Tuple[Deck, Deck, Deck, Deck]:
     """Deal the cards in the deck into four hands"""
     return (deck[0::4], deck[1::4], deck[2::4], deck[3::4])
 
