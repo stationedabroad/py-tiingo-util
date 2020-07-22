2157987e1d2f0ec71baac20a2c0ea382d5eaba32
sulman shaikh <sulmanmh@gmail.com>
2020-07-22 20:31:32 +0100

    mai nplay method for deck return type added

--- typing/deck.py
+++ typing/deck.py
@@ -23,7 +23,7 @@ def deal_hands(deck) -> Tuple[Deck, Deck, Deck, Deck]:
     """Deal the cards in the deck into four hands"""
     return (deck[0::4], deck[1::4], deck[2::4], deck[3::4])
 
-def play():
+def play() -> None:
     """Play a 4-player card game"""
     deck = create_deck(shuffle=True)
     names = "P1 P2 P3 P4".split()
