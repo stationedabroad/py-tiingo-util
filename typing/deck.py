7cb9a361d6c0c871b1e50cce896db265bd61604e
sulman shaikh <sulmanmh@gmail.com>
2020-07-23 22:54:10 +0100

    choose method added for basic return of element from seq

--- typing/deck.py
+++ typing/deck.py
@@ -23,6 +23,10 @@ def deal_hands(deck) -> Tuple[Deck, Deck, Deck, Deck]:
     """Deal the cards in the deck into four hands"""
     return (deck[0::4], deck[1::4], deck[2::4], deck[3::4])
 
+def choose(items):
+    return random.choice(items)
+
+
 def play() -> None:
     """Play a 4-player card game"""
     deck = create_deck(shuffle=True)
