7c444fb5e11438c6529c0c51421fa53e0d71fbf7
Sulman <sulmanmh@gmail.com>
2020-08-10 23:18:33 +0100

    typevar choose func example added

---
+++ typing/choose_func.py
@@ -0,0 +1,19 @@
+import random
+from typing import (
+        Sequence, 
+        TypeVar
+        )
+
+
+Choosable = TypeVar("Choosable")
+
+
+def choose(items: Sequence[Choosable]) -> Choosable:
+    return random.choice(items)
+
+
+names = ["Guido", "Jukka", "Ivan"]
+reveal_type(names)
+
+name = choose(names)
+reveal_type(name)
